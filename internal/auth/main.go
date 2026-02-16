abe35a076443db66b01bc36619522960ac0b4055
Mudassir Bilal <mughalmudassir966@gmail.com>
2026-02-16 14:45:48 +0500

    feat: added JWT verify function

--- internal/auth/main.go
+++ internal/auth/main.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -39,4 +40,36 @@ func CreateJWTToken(userID uuid.UUID, expiresIn time.Duration, secretKey string)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyJWT(jwtSecret, token string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	parsedToken, err := jwt.ParseWithClaims(token, &claims, func(t *jwt.Token) (any, error) {
+		return []byte(jwtSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	issuer, err := parsedToken.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if issuer != ISSUER {
+		return uuid.Nil, fmt.Errorf("issuer don't match")
+	}
+
+	rawId, err := parsedToken.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	id, err := uuid.Parse(rawId)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
 }
